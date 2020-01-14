2cc5384a7bfa2362be375ce55f60d285b8e45a7f
Halfhand Lorrine <hlorrine@vo-a2.21-school.ru>
2020-01-14 05:05:25 +0300

    dcs

---
+++ build/precache-manifest.e9229ff73a35c7b24e70eb06fedf7d2c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc6bfd002b5871590f6e9fe9cd365f94",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "e1ce641d9e98d597ab2a",
+    "url": "/my-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "eec426f5261f891a5ed0",
+    "url": "/my-app/static/css/main.b587710c.chunk.css"
+  },
+  {
+    "revision": "e1ce641d9e98d597ab2a",
+    "url": "/my-app/static/js/2.6a6306ba.chunk.js"
+  },
+  {
+    "revision": "140d1a208d28a5c60eae14e84c17f192",
+    "url": "/my-app/static/js/2.6a6306ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eec426f5261f891a5ed0",
+    "url": "/my-app/static/js/main.e0c8b8ef.chunk.js"
+  },
+  {
+    "revision": "41e4a3f5ebf2e1b0b7ac",
+    "url": "/my-app/static/js/runtime-main.d1d08ec2.js"
+  },
+  {
+    "revision": "192adb2444d482ce24acb243f1b2cfe1",
+    "url": "/my-app/static/media/avatar.192adb24.png"
+  },
+  {
+    "revision": "ab0c2625d45f5a45ceda45e61becfd36",
+    "url": "/my-app/static/media/background.ab0c2625.jpg"
+  },
+  {
+    "revision": "e4dcb7cbb79a961be25665b60942e601",
+    "url": "/my-app/static/media/login_checkmark.e4dcb7cb.svg"
+  },
+  {
+    "revision": "afd61c19cc62cf199b3802e14504af2d",
+    "url": "/my-app/static/media/login_error.afd61c19.png"
+  },
+  {
+    "revision": "816ddd9e1cb090850a55ecb4b3d099b4",
+    "url": "/my-app/static/media/logo_man.816ddd9e.png"
+  },
+  {
+    "revision": "3d3fde89b07a240aace21d9ab9a8a10b",
+    "url": "/my-app/static/media/sber_logo.3d3fde89.png"
+  },
+  {
+    "revision": "2183e5ee96040100fa342470c9f8e91d",
+    "url": "/my-app/static/media/weather_background.2183e5ee.jpg"
+  }
+]);
